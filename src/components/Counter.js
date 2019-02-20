fee8ac6fa69ce76b61d4c8bdc147ec2e1e8f8db3
Mitchell <soggybag@gmail.com>
2019-02-20 13:01:45 -0800

    initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>0</h1>
+        <p>
+          <button onClick={() => {}}> + </button>
+          <button onClick={() => {}}> + </button>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Counter
